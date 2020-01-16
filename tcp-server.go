bf9a39fbb2bd54bb1bc8fc77ca8a969ce5d4b85e
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2020-01-16 15:29:46 +0000

    Add missing tcp-server file
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

---
+++ tcp-server.go
@@ -0,0 +1,28 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "log"
+
+func main() {
+
+	fmt.Println("Launching server...")
+
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", ":8081")
+
+	// accept connection on port
+	conn, _ := ln.Accept()
+
+	// run loop forever (or until ctrl-c)
+	for {
+		// will listen for message to process ending in newline (\n)
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		// output message received
+		fmt.Print("Message Received:", string(message))
+	}
+}
